19a3bb555c887b36d04f173d8d924462452c4598
OmranK <omrankhoja@omrans-mbp.home>
2018-07-14 03:46:30 -0400

    Created a Card Deck Model and View

---
+++ PlayingCards/Deck.swift
@@ -0,0 +1,43 @@
+//
+//  Deck.swift
+//  PlayingCards
+//
+//  Created by Omran Khoja on 7/9/18.
+//  Copyright © 2018 AcronDesign. All rights reserved.
+//
+
+import Foundation
+
+struct Deck {
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }  
+    }
+    
+    mutating func draw() -> PlayingCard? {
+        if cards.count > 0 {
+            return cards.remove(at: cards.count.arc4random)
+        } else {
+            return nil
+        }
+    }
+}
+
+// MARK: Extension
+extension Int {
+    
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
+}
---
+++ myExtensions.swift
@@ -0,0 +1,57 @@
+//
+//  myExtensions.swift
+//  PlayingCards
+//
+//  Created by Omran Khoja on 7/14/18.
+//  Copyright © 2018 AcronDesign. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+//extension CGRect {
+//    var leftHalf: CGRect {
+//        return CGRect(x: minX, y: minY, width: width/2, height: height/2)
+//    }
+//
+//    var rightHalf: CGRect {
+//        return CGRect(x: minX, y: minY, width: width/2, height: height/2)
+//    }
+//
+//    func inset(by size: CGRect) -> CGRect {
+//        return insetBy(dx: size.width, dy: size.height)
+//    }
+//
+//    func sized(to size: CGSize) -> CGRect {
+//        return CGRect(origin: origin, size: size)
+//    }
+//
+//    func zoom(by scale: CGFloat) -> CGRect {
+//        let newWidth = width * scale
+//        let newHeight = height * scale
+//        return insetBy(dx: (width - newWidth), dy: (height - newHeight))
+//    }
+//}
+
+
+
+//extension CGPoint {
+//    func offsetBy(dx: CGFloat, dy: CGFloat) -> CGPoint{
+//        return CGPoint(x: x + dx, y: y + dy)
+//    }
+//}
+
+
+
+//extension Int {
+//    var arc4random: Int {
+//        if self > 0 {
+//            return Int(arc4random_uniform(UInt32(self)))
+//        } else if self < 0 {
+//            return -Int(arc4random_uniform(UInt32(self)))
+//        } else {
+//            return 0
+//        }
+//    }
+//}
